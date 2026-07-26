3e694517220e702089a5a23a10ed1f9c348155d0
viki441 <viki.k.mitova@gmail.com>
2026-07-26 13:52:17 +0300

    add stuff

---
+++ Website-project-catalog/js/paths.js
@@ -0,0 +1,8 @@
+const termoRolls = document.getElementById("termo-rolls");
+termoRolls.addEventListener("click", function()
+{
+
+    const container = document.getElementById("product-container");
+    prepareProduct(termoRolls);
+
+})
---
+++ Website-project-catalog/js/prepareProduct.js
@@ -0,0 +1,10 @@
+function prepareProduct(container)
+{
+    const mainDiv = document.createElement("div");
+    mainDiv.classList.add("s-12", "m-12", "l-4", "xl-3", "xxl-3");
+
+    const linkToProduct = document.createElement("a");
+    
+
+
+}
